2f7c00f1ba7fdffc24eb3f7ff24d1a03f163eb9c
luwg <luwg@zzlc.com>
2019-11-08 14:37:10 +0800

    init

---
+++ src/main/java/com/example/springbootbacklogdemo/SpringbootbacklogdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbacklogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbacklogdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootbacklogdemoApplication.class, args);
+    }
+
+}
